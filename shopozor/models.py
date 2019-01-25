f46e357c374be6f530426345cbc9143ff6b7dfbe
Laurent Michel <laurent.michel@softozor.ch>
2019-01-25 10:41:38 +0100

    Add shopozor permissions
    * provide data migrations containing the permissions
    * add shopozor permissions to the account app's permissions so that they appear in the graphql api

--- shopozor/models.py
+++ shopozor/models.py
@@ -1,17 +1,8 @@
 from django.db import models
 from saleor.core.permissions import MODELS_PERMISSIONS
-# from saleor.account.models import User
-# from django.contrib.auth.models import Permission
-# from django.contrib.contenttypes.models import ContentType
 
-MODELS_PERMISSIONS.append('shopozor.manage_producers')
-MODELS_PERMISSIONS.append('shopozor.manage_managers')
-MODELS_PERMISSIONS.append('shopozor.manage_rex')
-
-# ct = ContentType.objects.get_for_model(User)
-# Permission.objects.create(codename='manage_producers', name='Can manage producers', content_type=ct)
-# Permission.objects.create(codename='manage_managers', name='Can manage managers', content_type=ct)
-# Permission.objects.create(codename='manage_rex', name='Can manage rex', content_type=ct)
+MODELS_PERMISSIONS.append('account.manage_producers')
+MODELS_PERMISSIONS.append('account.manage_managers')
+MODELS_PERMISSIONS.append('account.manage_rex')
 
 # Create your models here.
-print('wooooooo')
