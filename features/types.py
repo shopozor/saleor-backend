4406ff267f7914bf612c0792a680feb7b6d5621d
Laurent Michel <laurent.michel@softozor.ch>
2019-01-30 12:10:19 +0100

    Use timedelta to compute time units

--- features/types.py
+++ features/types.py
@@ -1,5 +1,6 @@
 from behave import register_type
 import parse
+from datetime import timedelta
 
 
 @parse.with_pattern(r'client|administrateur')
@@ -27,27 +28,20 @@ register_type(ValidityType=parse_user_credentials_validity)
 
 
 def unit_to_seconds(unit):
-    SECONDS_IN_ONE_SECOND = 1
-    SECONDS_IN_ONE_MINUTE = 60
-    SECONDS_IN_ONE_HOUR = SECONDS_IN_ONE_MINUTE * 60
-    SECONDS_IN_ONE_DAY = SECONDS_IN_ONE_HOUR * 24
-    SECONDS_IN_ONE_WEEK = SECONDS_IN_ONE_DAY * 7
-    SECONDS_IN_ONE_MONTH = SECONDS_IN_ONE_DAY * 30
-    SECONDS_IN_ONE_YEAR = SECONDS_IN_ONE_DAY * 360
     switch = {
-        'secondes': SECONDS_IN_ONE_SECOND,
-        'seconde': SECONDS_IN_ONE_SECOND,
-        'minutes': SECONDS_IN_ONE_MINUTE,
-        'minute': SECONDS_IN_ONE_MINUTE,
-        'heures': SECONDS_IN_ONE_HOUR,
-        'heure': SECONDS_IN_ONE_HOUR,
-        'jours': SECONDS_IN_ONE_DAY,
-        'jour': SECONDS_IN_ONE_DAY,
-        'semaines': SECONDS_IN_ONE_WEEK,
-        'semaine': SECONDS_IN_ONE_WEEK,
-        'mois': SECONDS_IN_ONE_MONTH,
-        'ans': SECONDS_IN_ONE_YEAR,
-        'an': SECONDS_IN_ONE_YEAR
+        'secondes': 1,
+        'seconde': 1,
+        'minutes': timedelta(minutes=1).seconds(),
+        'minute': timedelta(minutes=1).seconds(),
+        'heures': timedelta(hours=1).seconds(),
+        'heure': timedelta(hours=1).seconds(),
+        'jours': timedelta(days=1).seconds(),
+        'jour': timedelta(days=1).seconds(),
+        'semaines': timedelta(weeks=1).seconds(),
+        'semaine': timedelta(weeks=1).seconds(),
+        'mois': timedelta(days=30).seconds(),
+        'ans': timedelta(days=360).seconds(),
+        'an': timedelta(days=360).seconds()
     }
     return switch[unit]
 
