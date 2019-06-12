d81c743952e0f181ff31e9ed99b6a622a90d5566
Laurent Michel <laurent.michel@softozor.ch>
2019-06-12 11:54:59 +0200

    improve password policy test

--- features/steps/passwordPolicy.py
+++ features/steps/passwordPolicy.py
@@ -5,13 +5,10 @@ from django.core.exceptions import ValidationError
 
 @given(u'le mot de passe {password}')
 def step_impl(context, password):
-    context.password_error = None
-    try:
-        validate_password(password)
-    except Exception as e:
-        context.password_error = e
+    context.password = password
 
 
 @then(u'il est non conforme')
 def step_impl(context):
-    assert isinstance(context.password_error, ValidationError)
+    with context.test.assertRaises(ValidationError):
+        validate_password(context.password)
