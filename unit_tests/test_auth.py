69f26b6f09fc4f274efb1a098d726fa12a9dcb53
Laurent Michel <laurent.michel@softozor.ch>
2019-06-20 07:17:12 +0200

    clean up initial unit tests

--- unit_tests/test_auth.py
+++ unit_tests/test_auth.py
@@ -24,5 +24,3 @@ def test_encoded_userid_should_be_decoded_from_url(user_id):
     encoded_user_id = urlsafe_base64_encode(force_bytes(user_id))
     decoded_user_id = force_text(urlsafe_base64_decode(encoded_user_id))
     assert user_id == int(decoded_user_id)
-
-# TODO: test that the send password reset email uses the above encoded_user_id
