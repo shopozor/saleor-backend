379b837bc39b09756052ae1b0e88042409bbd8e4
Laurent Michel <laurent.michel@softozor.ch>
2019-07-20 15:52:08 +0200

    * add initial step code for consultation of a shop's catalogue * update graphql submodule

--- features/steps/consumerVisitsShop.py
+++ features/steps/consumerVisitsShop.py
@@ -10,10 +10,27 @@ def query_shops(client):
     return get_graphql_content(response)
 
 
+def query_shop_catalogue(client, shop_id):
+    query = get_query_from_file('shopCatalogue.graphql')
+    variables = {
+        'shopId': shop_id
+    }
+    response = client.post_graphql(query, variables)
+    return get_graphql_content(response)
+
+
 @when(u'Incognito demande quels Shops il peut visiter')
 def step_impl(context):
     test_client = context.test.client
     context.response = query_shops(test_client)
+
+
+@when(u'Incognito visite un Shop')
+def step_impl(context):
+    shop = Shop.objects.first()
+    context.selected_shop = shop
+    test_client = context.test.client
+    context.response = query_shop_catalogue(test_client, shop.id)
 
 
 @then(u'il obtient pour chaque Shop disponible ses coordonnées géographiques avec sa description générale')
@@ -24,5 +41,14 @@ def step_impl(context):
 
     for edge in shop_edges_data:
         shop_data = edge['node']
-        context.test.assertTrue(expected_shops.filter(name=shop_data['name'], description=shop_data['description'],
+        context.test.assertTrue(expected_shops.filter(pk=shop_data['id'], name=shop_data['name'], description=shop_data['description'],
                                                       latitude=shop_data['geocoordinates']['latitude'], longitude=shop_data['geocoordinates']['longitude']).exists())
+
+
+@then(u'il obtient la liste de tous les Produits qui y sont vendus')
+def step_impl(context):
+    # 0. prepare product fixtures to allow for this step!
+    # 1. get selected shop from context
+    # 2. get products from selected shop
+    # 3. compare products' properties with those received in the response (id, name, variants, category, productType)
+    raise NotImplementedError('step not yet implemented')
