6bdb2ea5f896c9ec10575aa841c7eca8065cdd78
Laurent Michel <laurent.michel@softozor.ch>
2019-08-08 16:57:03 +0200

    try to retrieve the tag

--- production/deploy.groovy
+++ production/deploy.groovy
@@ -4,12 +4,13 @@ pipeline {
     DOCKER_CREDENTIALS = credentials('docker-credentials')
     // TODO: put tag here instead of "latest"!
     DOCKER_REPO = "softozor/shopozor-backend:latest"
+    TEST_VAR = "blabla:$TAG"
   }
   stages {
     stage('Publish docker container') {
       steps {
         script {
-            sh 'env'
+            sh 'echo $TAG'
             sh "cp production/Dockerfile production/.dockerignore ."
             sh "docker login -u $DOCKER_CREDENTIALS_USR -p $DOCKER_CREDENTIALS_PSW"
             sh "docker build -t $DOCKER_REPO ."
