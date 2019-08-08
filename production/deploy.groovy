8b0861393c0dc0637ce8f62ac12d7c6fad1b4c31
Laurent Michel <laurent.michel@softozor.ch>
2019-08-08 17:00:33 +0200

    Add job to create production backend docker image #22
    * finalize publishing of container script
    cf. https://github.com/shopozor/ci/issues/22

--- production/deploy.groovy
+++ production/deploy.groovy
@@ -2,15 +2,12 @@ pipeline {
   agent any
   environment {
     DOCKER_CREDENTIALS = credentials('docker-credentials')
-    // TODO: put tag here instead of "latest"!
-    DOCKER_REPO = "softozor/shopozor-backend:latest"
-    TEST_VAR = "blabla:$TAG"
+    DOCKER_REPO = "softozor/shopozor-backend:$TAG"
   }
   stages {
     stage('Publish docker container') {
       steps {
         script {
-            sh 'echo $TEST_VAR'
             sh "cp production/Dockerfile production/.dockerignore ."
             sh "docker login -u $DOCKER_CREDENTIALS_USR -p $DOCKER_CREDENTIALS_PSW"
             sh "docker build -t $DOCKER_REPO ."
