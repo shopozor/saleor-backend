337840b53dcdb25b0edd52f8eb8ee7325ab442d6
Laurent Michel <laurent.michel@softozor.ch>
2019-08-09 09:34:15 +0200

    Deploy production system automatically
    * add deployment scripts
    * complete jenkins pipeline
    
    cf. https://github.com/shopozor/ci/issues/22

--- production/deploy.groovy
+++ production/deploy.groovy
@@ -15,5 +15,18 @@ pipeline {
         }
       }
     }
+    stage('Deploy environment') {
+      environment {
+        JELASTIC_APP_CREDENTIALS = credentials('jelastic-app-credentials')
+        JELASTIC_CREDENTIALS = credentials('jelastic-credentials')
+        PRODUCTION_ENV_NAME = "shopozor-backend"
+        PATH_TO_JPS = "./production/manifest.jps"
+      }
+      steps {
+        script {
+          sh "./production/deploy-to-jelastic.sh $JELASTIC_APP_CREDENTIALS_USR $JELASTIC_APP_CREDENTIALS_PSW $JELASTIC_CREDENTIALS_USR $JELASTIC_CREDENTIALS_PSW $PRODUCTION_ENV_NAME cp $PATH_TO_JPS $TAG"
+        }
+      }
+    }
   }
 }
