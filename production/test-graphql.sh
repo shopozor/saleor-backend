44cc637ff3f50ce80b3b20945c5316cbcaf5ba8c
Laurent Michel <laurent.michel@softozor.ch>
2019-08-12 13:03:55 +0200

    finish test script for graphql endpoint

--- production/test-graphql.sh
+++ production/test-graphql.sh
@@ -9,16 +9,26 @@ GRAPHQL_URL=$1
 
 EXPECTED_RESPONSE='{"errors": [{"message": "You do not have permission to perform this action", "locations": [{"line": 1, "column": 9}], "path": ["me"]}], "data": {"me": null}}'
 
-# TODO: 1. curl during 5 minutes until it does not return 502
+TIME_OUT_IN_SECONDS=300
+TIMER_DELTA_IN_SECONDS=5
+START_TIME=`date +%s`
 while [[ $status -ne 200 ]] ; do
-    cmd=$(curl -H "Content-Type: application/json" \
-        -w "#%{http_code}" \
+    status=$(curl -H "Content-Type: application/json" \
+        -w "%{http_code}" \
         -d '{ "query": "query { me { id } }", "variables": "{}" }' \
-        -s \
+        -so /dev/null \
         -X POST \
         $GRAPHQL_URL)
-    status=$(echo $cmd | cut -d '#' -f 2)
+    CURRENT_TIME=`date +%s`
+    RUNTIME=$((CURRENT_TIME - START_TIME))
+    [ $RUNTIME -ge $TIME_OUT_IN_SECONDS ] && exit 1
+    sleep $TIMER_DELTA_IN_SECONDS
 done
 
-body=$(echo $cmd | cut -d '#' -f 1)
+body=$(curl -H "Content-Type: application/json" \
+    -d '{ "query": "query { me { id } }", "variables": "{}" }' \
+    -s \
+    -X POST \
+    $GRAPHQL_URL)
+
 [ "$EXPECTED_RESPONSE" == "$body" ] && exit 0 || exit 1
