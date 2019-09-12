acf45946ff4e6ca72171ec846ded956806556917
Laurent Michel <laurent.michel@softozor.ch>
2019-09-12 08:47:38 +0200

    clean up comments

--- features/management/commands/generate_graphql_responses.py
+++ features/management/commands/generate_graphql_responses.py
@@ -1,15 +1,9 @@
-# from copy import deepcopy
 from django.conf import settings
 from django.core.management.base import BaseCommand
-# from features.utils.fixtures import json
 from features.utils.graphql.responses_generator import ShopCategoriesGenerator, ShopListsGenerator, ProductListsGenerator
-
-# import os
 
 
 def generate_responses_for_variant(output_folder, variant):
-    # os.makedirs(os.path.join(output_folder, variant), exist_ok=True)
-
     generator = ShopListsGenerator(output_folder, variant)
     generator.generate()
 
@@ -18,11 +12,6 @@ def generate_responses_for_variant(output_folder, variant):
 
     generator = ProductListsGenerator(output_folder, variant)
     generator.generate()
-
-    # shop_catalogues, product_details = generate_shop_catalogues(
-    #     variant)
-    # output_shop_catalogues(shop_catalogues, output_folder, variant)
-    # output_product_details(product_details, output_folder, variant)
 
 
 class Command(BaseCommand):
@@ -37,7 +26,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         output_folder = options['output_folder']
         fixture_variant = options['fixture_variant']
-        # os.makedirs(output_folder, exist_ok=True)
 
         if fixture_variant == 'all':
             for variant in 'small', 'medium', 'large':
