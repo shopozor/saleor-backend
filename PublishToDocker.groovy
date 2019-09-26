827faa952dba0ee11731016ea09286f8f92b5530
Laurent Michel <laurent.michel@softozor.ch>
2019-09-26 12:32:06 +0200

    refactor pipeline

--- PublishToDocker.groovy
+++ PublishToDocker.groovy
@@ -5,11 +5,17 @@ pipeline {
     DOCKER_REPO = "softozor/shopozor-$REPO:$BRANCH"
   }
   stages {
-    stage('Publish staging docker container') {
+    stage('Build docker image') {
       steps {
         script {
             sh "docker login -u $DOCKER_CREDENTIALS_USR -p $DOCKER_CREDENTIALS_PSW"
             sh "docker build --build-arg ENABLE_DEV_TOOLS=$ENABLE_DEV_TOOLS --network=host -t $DOCKER_REPO ."
+        }
+      }
+    }
+    stage('Publish docker image') {
+      steps {
+        script {
             sh "docker push $DOCKER_REPO"
         }
       }
