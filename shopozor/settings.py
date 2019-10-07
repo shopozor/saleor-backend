2c0f73b7c64f48783c9765c83f8ac4fb8345c08d
Laurent Michel <laurent.michel@softozor.ch>
2019-10-07 13:06:05 +0200

    * introduce the EmailSender class * introduce the smtp configuration model * adapt the tests * cleanup manifest

--- shopozor/settings.py
+++ shopozor/settings.py
@@ -51,12 +51,5 @@ WSGI_APPLICATION = "shopozor.wsgi.application"
 
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 
-if EMAIL_BACKEND == 'django.core.mail.backends.smtp.EmailBackend':
-    EMAIL_HOST = os.getenv('EMAIL_HOST')
-    EMAIL_PORT = os.getenv('EMAIL_PORT')
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False)
-    EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False)
-elif EMAIL_BACKEND == 'django.core.mail.backends.filebased.EmailBackend':
+if EMAIL_BACKEND == 'django.core.mail.backends.filebased.EmailBackend':
     EMAIL_FILE_PATH = os.getenv('EMAIL_FILE_PATH')
