54dcd8ad5a0249c0a81454aef82b300a58a5f99b
Laurent Michel <laurent.michel@softozor.ch>
2019-10-14 18:08:45 +0200

    fix migrations

--- shopozor/migrations/0014_emailsmtpconfiguration.py
+++ shopozor/migrations/0014_emailsmtpconfiguration.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shopozor', '0009_staff_description'),
+        ('shopozor', '0013_initial_vat_values'),
     ]
 
     operations = [
